d478e0a8f942ab620db173b23a8ce1e01988c1a5
Alex <sokod7514@gmail.com>
2020-04-13 22:01:57 +0300

    Finished Week 5

---
+++ Dynamic programming/Discrete Knapsack with repetition/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Programming
+{
+    class Item
+    {
+        public int weight { get; private set; }
+        public int value { get; private set; }
+        public string name { get; private set; }
+        public Item(int weight, int value,string name)
+        {
+            this.weight = weight;
+            this.value = value;
+            this.name = name;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the maximum capacity?");
+            int capacity = int.Parse(Console.ReadLine());
+            Item[] items = new Item[] {new Item(6, 30,"meat"), new Item(3, 14,"cheese"), new Item(4, 16,"bread"), new Item(2, 9,"water") };
+            List<Item>[] inKnapsack;
+            int best = Knapsack(capacity, items,out inKnapsack);
+            Console.WriteLine($"Complectation is finished. Value of {best} is best. You should take:");
+            for (int i = 0; i < inKnapsack[capacity].Count; i++)
+            {
+                Console.Write(inKnapsack[capacity][i].name+";");
+            }
+            Console.ReadKey();
+        }
+
+        public static int Knapsack(int weight,Item[] items, out List<Item>[] inKnapsack)
+        {
+            int[] values = new int[weight+1];
+            inKnapsack = new List<Item>[weight + 1];
+            inKnapsack[0] = new List<Item>(0);
+            for (int w = 1; w < values.Length; w++)
+            {
+                values[w] = 0;
+                inKnapsack[w] = new List<Item>(items.Length);
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if (items[i].weight<=w)
+                    {
+                        int value = values[w - items[i].weight] + items[i].value;
+                        if (value > values[w])
+                        {
+                            values[w] = value;
+                            inKnapsack[w].Clear();
+                            inKnapsack[w].AddRange(inKnapsack[w - items[i].weight]);
+                            inKnapsack[w].Add(items[i]);
+                        }
+                    }
+                }
+            }
+            return values[weight];
+        }
+
+    }
+} 
---
+++ Dynamic programming/Editing Distance/Editing distance.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            EditDistance(first, second);
+            Console.ReadKey();
+        }
+
+        private static void EditDistance(string first, string second)
+        {
+            char[] f = first.ToCharArray();
+            char[] s = second.ToCharArray();
+            int[,] dist = new int[f.Length+1,s.Length+1];
+            //filling in header
+            for (int i = 0; i < dist.GetLength(0); i++)
+            {
+                dist[i, 0] = i;
+            }
+            for (int j = 0; j < dist.GetLength(1); j++)
+            {
+                dist[0, j] = j;
+            }
+
+            for (int i = 1; i < dist.GetLength(0); i++)
+            {
+                for (int j = 1; j < dist.GetLength(1); j++)
+                { 
+                    if (f[i-1]==s[j-1])
+                    {
+                        //insertion,deletion,match
+                        dist[i, j] = Min(dist[i, j - 1] + 1, dist[i - 1, j] + 1, dist[i - 1, j - 1]);
+                    }
+                    else
+                    {
+                        //insertion,deletion,mismatch
+                        dist[i, j] = Min(dist[i, j - 1] + 1, dist[i - 1, j] + 1, dist[i - 1, j - 1] + 1);
+                    }
+                }
+            }
+            Console.WriteLine(dist[dist.GetLength(0) - 1, dist.GetLength(1) - 1]);
+        }
+
+        private static int Min(int v1, int v2, int v3)
+        {
+            return Math.Min(Math.Min(v1, v2), v3);
+        }
+    }
+}
