adb8eee371d42d40fba55c23b7b79646cf894faf
Alex <sokod7514@gmail.com>
2020-04-17 21:18:12 +0300

    Optimized placing parentheses

--- Dynamic programming/Maximize the Value of an Arithmetic Expression/placing parentheses.cs
+++ Dynamic programming/Maximize the Value of an Arithmetic Expression/placing parentheses.cs
@@ -18,44 +18,41 @@ namespace Dynamic_Programming
         public static long Parentheses(string expression)
         {
             char[] op = new char[] { '-', '+', '*' };
-            char[] digits = new char[] { '0','1','2','3','4','5','6','7','8','9'};
+            char[] digits = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
             string[] numbers = expression.Split(op);
             string[] operations = expression.Split(digits);
 
-            long[,] Min = new long[numbers.Length+1, numbers.Length+1];
-            long[,] Max = new long[numbers.Length+1, numbers.Length+1];
-            for (int i = 1; i < Min.GetLength(0); i++)
+            long[,] Min = new long[numbers.Length, numbers.Length];
+            for (int i = 0; i < Min.GetLength(0); i++)
             {
-                Min[i, i] = int.Parse(numbers[i-1]);
-                Max[i, i] = int.Parse(numbers[i-1]);
+                Min[i, i] = int.Parse(numbers[i]);
             }
             int j;
-            for (int s = 1; s < numbers.Length+1; s++)
+            for (int s = 0; s < numbers.Length; s++)
             {
-                for (int i = 1; i < numbers.Length+1-s; i++)
+                for (int i = 0; i < numbers.Length - s - 1; i++)
                 {
-                    j = i + s;
-                    long[] min_max = MinAndMax(i, j, Min, Max, operations);
+                    j = i + s + 1;
+                    long[] min_max = MinAndMax(i, j, Min, operations);
                     Min[i, j] = min_max[0];
-                    Max[i, j] = min_max[1];
+                    Min[j, i] = min_max[1];
                 }
             }
-            return Max[1,numbers.Length];
-            throw new NotImplementedException();
+            return Min[numbers.Length - 1,0];
         }
 
-        public static long[] MinAndMax(int i, int j, long[,] Min, long[,] Max, string[] op)
+        public static long[] MinAndMax(int i, int j, long[,] Min, string[] op)
         {
             long min = long.MaxValue;
             long max = long.MinValue;
             long a, b, c, d;
-            for (int k = i; k <= j-1; k++)
+            for (int k = i; k <= j - 1; k++)
             {
-                a = eval(Max[i, k], Max[k + 1, j], op[k]);
-                b = eval(Max[i, k], Min[k + 1, j], op[k]);
-                c = eval(Min[i, k], Max[k + 1, j], op[k]);
-                d = eval(Min[i, k], Min[k + 1, j], op[k]);
+                a = eval(Min[k,i], Min[j,k+1], op[k + 1]);
+                b = eval(Min[k,i], Min[k + 1, j], op[k + 1]);
+                c = eval(Min[i, k], Min[j,k+1], op[k + 1]);
+                d = eval(Min[i, k], Min[k + 1, j], op[k + 1]);
                 min = Math.Min(Math.Min(Math.Min(min, a), c), d);
                 max = Math.Max(Math.Max(Math.Max(max, a), c), d);
             }
@@ -83,4 +80,4 @@ namespace Dynamic_Programming
         }
 
     }
-} 
+}
