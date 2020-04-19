9100bf21f2f1460bdf9936dc3c35605b828ef06a
Alex <sokod7514@gmail.com>
2020-04-19 12:17:38 +0300

    Initializing Data structures

---
+++ Data structures/Basics/Check Brackets/Check Brackets.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] inputs = Console.ReadLine().ToCharArray();
+            Console.WriteLine(Equal(inputs));
+            Console.ReadKey();
+        }
+
+        private static string Equal(char[] inputs)
+        {
+            char[] open = new char[] { '{', '[', '(' };
+            char[] closes = new char[] { '}', ']', ')' };
+            Stack<char> stack = new Stack<char>();
+            Stack<int> enumerator = new Stack<int>();
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                for (int j = 0; j < open.Length; j++)
+                {
+                    if (inputs[i] == open[j])
+                    {
+                        stack.Push(inputs[i]);
+                        enumerator.Push(i + 1);
+                        break;
+                    }
+                    if (inputs[i] == closes[j])
+                    {
+                        if (stack.Empty())
+                            return (i+1).ToString();
+
+                        char value = stack.Pop();
+
+                        if (value != open[j])
+                            return (i+1).ToString();
+                        else enumerator.Pop();
+                        break;
+                    }
+                    if (j == open.Length-1)
+                    {
+                        enumerator.Push(i + 1);
+                    }
+                }
+            }
+            if (stack.Empty())
+                return "Success";
+            else return enumerator.Pop().ToString();
+        }
+    }
+}
---
+++ Data structures/Basics/Check Brackets/Structures.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures
+{
+    class LinkedList<T>
+    {
+        private class Node
+        {
+            private T _data;
+            public T data => _data;
+            private Node _next;
+            public Node next
+            {
+                get => _next;
+                set => _next = value;
+            }
+            public Node(T value)
+            {
+                next = null;
+                _data = value;
+            }
+        }
+        private Node head;
+        public LinkedList()
+        {
+            head = null;
+        }
+        public void PushFront(T value)
+        {
+            Node n = new Node(value);
+            n.next = head;
+            head = n;
+        }
+        public T TopFront()
+        {
+            return head.data;
+        }
+        public T PopFront()
+        {
+            if (Empty()) throw new NullReferenceException("List is empty");
+            T value = head.data;
+            head = head.next;
+            return value;
+        }
+        public bool Empty()
+        {
+            return head == null ? true : false;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = head;
+            while(current!=null)
+            {
+                yield return current.data;
+                current = current.next;
+            }
+        }
+
+
+    }
+    class Stack<T>
+    {
+        private LinkedList<T> list;
+
+        public Stack()
+        {
+            list = new LinkedList<T>();
+        }
+        public void Push(T value)
+        {
+            list.PushFront(value);
+        }
+        public T Pop()
+        {
+            return list.PopFront();
+        }
+
+        public T Top()
+        {
+            return list.TopFront();
+        }
+        public bool Empty()
+        {
+            return list.Empty();
+        }
+
+    }
+
+}
