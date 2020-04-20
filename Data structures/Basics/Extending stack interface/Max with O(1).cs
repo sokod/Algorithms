b094d66fc3fa95ef4394bfd800d443fe0ff6da2c
Alex <sokod7514@gmail.com>
2020-04-20 17:52:05 +0300

    Doing basics

---
+++ Data structures/Basics/Extending stack interface/Max with O(1).cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures
+{
+
+
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            int queries = int.Parse(Console.ReadLine());
+            Stack stack = new Stack();
+            string[] inputs = new string[queries];
+            for (int i = 0; i < queries; i++)
+            {
+                inputs[i] = Console.ReadLine();
+            }
+            foreach (string input in inputs)
+            {
+                if (input.Length>3)
+                {
+                    stack.Push(int.Parse(input.Substring(4)));
+                }
+                else
+                {
+                    if (input == "max")
+                        Console.WriteLine(stack.Max());
+                    else if (input == "pop")
+                        stack.Pop();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Data structures/Basics/Extending stack interface/Structures.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures
+{
+    class LinkedList<T>
+    {
+        private class Node
+        {
+            private T _data;
+            public T data => _data;
+            private Node _next;
+            public Node next
+            {
+                get => _next;
+                set => _next = value;
+            }
+            public Node(T value)
+            {
+                next = null;
+                _data = value;
+            }
+        }
+        private Node head;
+        public LinkedList()
+        {
+            head = null;
+        }
+        public void PushFront(T value)
+        {
+            Node n = new Node(value);
+            n.next = head;
+            head = n;
+        }
+        public T TopFront()
+        {
+            return head.data;
+        }
+        public T PopFront()
+        {
+            if (Empty()) throw new NullReferenceException("List is empty");
+            T value = head.data;
+            head = head.next;
+            return value;
+        }
+        public bool Empty()
+        {
+            return head == null ? true : false;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node current = head;
+            while(current!=null)
+            {
+                yield return current.data;
+                current = current.next;
+            }
+        }
+
+
+    }
+    class Stack
+    {
+        private LinkedList<int> list;
+        private LinkedList<int> max;
+        public Stack()
+        {
+            list = new LinkedList<int>();
+            max = new LinkedList<int>();
+        }
+        public void Push(int value)
+        {
+            if (max.Empty())
+                max.PushFront(value);
+            else if (value >= max.TopFront()) 
+                max.PushFront(value);
+            list.PushFront(value);
+        }
+        public int Pop()
+        {
+            int popped = list.PopFront();
+            if (popped == max.TopFront())
+                max.PopFront();
+            return popped;
+        }
+
+        public int Top()
+        {
+            return list.TopFront();
+        }
+        public bool Empty()
+        {
+            return list.Empty();
+        }
+        public int Max()
+        {
+            return max.TopFront();
+        }
+
+    }
+
+}
